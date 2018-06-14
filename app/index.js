0f57e932425d1da3f3eb0fbb54c1153a42a81fbf
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-06-14 12:02:39 -0700

    added more functionality to classes

--- app/index.js
+++ app/index.js
@@ -4,3 +4,10 @@ import SuperPerson from './person';
 const peter = new SuperPerson('Peter', 'SUPER BUNDURU');
 addParagraph( peter.talk('BUNDURU!') );
 addParagraph( peter.saySuperPower());
+
+const jhon = new SuperPerson('Jhon', 'ULTRA BUNDURU');
+addParagraph( jhon.talk('ME TOO'));
+addParagraph( jhon.saySuperPower());
+
+addParagraph( peter.dance());
+addParagraph( jhon.attack(peter));
--- app/person.js
+++ app/person.js
@@ -7,6 +7,13 @@ class Person {
     talk( text ){
         return `${ this.name } says: ${ text }`;
     }
+    greeting( text ){
+        return `Hello! my name is ${ this.name }`;
+    }
+    dance(){
+        return `${this.name} is now dancing`;
+    }
+
 }
 
 class SuperPerson extends Person {
@@ -17,6 +24,19 @@ class SuperPerson extends Person {
     saySuperPower(){
         return this.talk( `I can find ${this.superpower}` );
     }
+    attack( target ){
+        let speech = `${this.name} attacks: HAHA! I slapped ${target.name}`
+        this.talk(speech);
+        target.damaged(this.name);
+        return speech;
+    }
+    damaged( targetName ){
+        let speech = `${this.name}says: OUCH! ${targetName} SLAPPED ME!`
+        this.talk(speech);
+        return speech;
+    }
 }
 
 export default SuperPerson;
+
+//add two more functions to both classes.
